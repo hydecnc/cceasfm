4f10ec817e275ee5099acf367a16361768b85e71
hydecnc <seokjinyoo0716@gmail.com>
2023-09-20 07:26:31 -0600

    added better phone support

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -6,8 +6,10 @@
 	// 	{ name: 'Mas Utiles', date: 'MON & WED', volunteerPositions: 7, hoursRewarded: 13, },
 	// 	{ name: 'A Roof in ASFM November Construction', date: '', volunteerPositions: 112, hoursRewarded: 22, }
 	// ];
-	import { app, db } from '$lib/firebase';
+	import { app, db, storage } from '$lib/firebase';
 	import { collection, getDocs } from 'firebase/firestore';
+	import { getDownloadURL, ref } from 'firebase/storage';
+	import { onMount } from 'svelte';
 
 	/**
 	 * @type {import("@firebase/firestore").DocumentData[]}
@@ -28,6 +30,23 @@
 	};
 
 	let promise = getQuerySnapshot();
+
+	/**
+	 * @type {string}
+	 */
+	// let downloadUrl;
+	// const downloadImage = async (fileName) => {
+	// 	try {
+	// 		const fileRef = ref(storage, "activities/fotomasutiles.png");
+	// 		downloadUrl = await getDownloadURL(fileRef);				
+	// 	} catch (error) {
+	// 		console.error('Error downloading file:', error);
+	// 	}
+	// }
+
+	// onMount(() => {
+	// 	downloadImage();
+	// })
 </script>
 
 <div class="flex flex-col justify-center items-center space-y-8">
@@ -35,11 +54,12 @@
 	>
 	<div class="text-7xl">ACTIVITES</div>
 
-	<div class="grid grid-cols-1 w-4/5 gap-5 lg:grid-cols-3">
+	<div class="grid grid-cols-1 w-4/5 gap-5 md:grid-cols-2 lg:grid-cols-3">
 		{#await promise then querySnapshot}
 			{#each querySnapshot.docs as activity, i}
 				<a class="card p-4 card-hover flex flex-col" href={`/activities/${id[i]}`}>
 					<img src="/fotomasutiles.png" alt="masutiles" class="pb-5 rounded-md" />
+					<!-- <img src={downloadUrl(activity.data().name)} alt="masutiles" class="pb-5 rounded-md" /> -->
 					<div class="h3 font-bold">
 						{activity.data().name}
 					</div>
